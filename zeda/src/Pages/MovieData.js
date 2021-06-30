0a944a64a4a660360b5a8c7d49d4d8bbf46eaee6
Mahima <mahi19071997@gmail.com>
2021-07-01 02:40:21 +0530

    ratings/year/ui

---
+++ zeda/src/Pages/MovieData.js
@@ -0,0 +1,45 @@
+import styled from "styled-components"
+
+export const Moviecard=styled.div`
+display:flex;
+justify-content:space-between;
+margin: 10% auto;
+width:70%;
+background-color:#E4ffff;
+padding:0.7%;
+font-family: 'Sans','Helvetica Neue',Helvetica,Arial,sans-serif;`
+
+export const Title=styled.div`
+font-size:35px;
+line-height:30px;
+font-Weight:700;
+margin-top:3%;
+
+`
+
+export const Runtime=styled.div`
+font-size:20px;
+line-height:26px;
+font-Weight:600;
+margin:3% auto;
+display: flex;
+justify-content:space-between;
+flex-direction:row;
+width: 80%;
+`
+
+export const Plot=styled.div`
+font-size:15px;
+text-align:left;
+line-height:26px;
+font-Weight:500;
+margin:3% 5%;
+
+`
+export const Leads=styled.div`
+font-size:13px;
+line-height:26px;
+font-Weight:500;
+margin:4% 5%;
+
+`
--- zeda/src/Pages/MovieData.jsx
+++ zeda/src/Pages/MovieData.jsx
@@ -1,24 +1,52 @@
-import React from 'react'
-import {useParams} from "react-router-dom"
-import {useDispatch,useSelector} from "react-redux"
-import {getSingleData} from './../Redux/appRedux/action'
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getSingleData } from "./../Redux/appRedux/action";
+import { Moviecard, Title, Runtime, Plot } from "./MovieData";
+import { Rating } from "@material-ui/lab";
 
 export function MovieData() {
-    const {id}=useParams()
-    const movie= useSelector(state=>state.app.movie)
-    console.log(movie.Title)
-    const dispatch =useDispatch()
-    React.useEffect(()=>{
-        dispatch(getSingleData(id))
-    },[])
-    return (
-        
-        <div>
-            <div>{movie?.imdbID}</div>
-            <div>{movie?.Awards}</div>
-            <div>bug</div>
-            
-        </div>
-    )
+  const { id } = useParams();
+  const movie = useSelector((state) => state.app.movie);
+
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(getSingleData(id));
+  }, []);
+  return (
+    <>
+      {movie && (
+        <Moviecard>
+          <div>
+            <img src={movie.Poster} alt="" />
+          </div>
+
+          <div>
+            <Title>{movie.Title}</Title>
+            <Runtime>
+              {/* <div>{JSON.parse(JSON.stringify(movie?.DVD.split(" ")[2]))}</div> */}
+              <div>{movie.Runtime}</div>
+              <div>{movie.Genre}</div>
+            </Runtime>
+            <Plot>{movie.Plot}</Plot>
+            <div>IMDB Rating {movie.imdbRating}</div>
+            <Rating
+              value={movie.imdbRating}
+              precision={0.5}
+              max={10}
+            />
+            <div>Main Leads {movie.Actors}</div>
+            <div>{movie.Awards}</div>
+            <div>{movie.BoxOffice}</div>
+
+            <div>{movie.Director}</div>
+
+            <div>{movie.Language}</div>
+
+            <div>{movie.Production}</div>
+          </div>
+        </Moviecard>
+      )}
+    </>
+  );
 }
